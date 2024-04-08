5899e7a22fb92e04473a16157843c5b7075033b3
Omeche <omechetochi@gmail.com>
2024-04-08 09:55:17 -0700

    fanta

--- testcodee.py
+++ testcodee.py
@@ -2,5 +2,6 @@ response = input('Are you thirsty?')
 if response == 'yea' or response == 'yes':
     print('Drink Water')
     print('Drink coke')
+    print('Drink fanta')
 else:
     print("Go and play")
